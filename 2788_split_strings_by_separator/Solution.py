12a1da7d2d2e5ce6b94e0e3b01a8fb362ca58557
moemaair <rageali12@gmail.com>
2023-09-03 23:25:19 +0300

    Runtime: 110ms space 16.34MB

---
+++ 2788_split_strings_by_separator/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    
+    def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
+        ls = [] 
+        for data in words:
+            word = data.split(separator)
+            for data in word:
+                if(data != ""):
+                    ls.append(data)
+        return ls

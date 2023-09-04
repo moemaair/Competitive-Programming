88a8e3915a3777dc57d6561a7a417391381cb0c8
moemaair <rageali12@gmail.com>
2023-09-04 13:02:03 +0300

    /opt/homebrew/bin/python3

---
+++ make_string_great/Solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+
+        for c in s:
+            poped = ""
+            if stack and abs(ord(stack[-1]) - ord(c)) == 32: #
+                poped = stack.pop()
+
+            stack.append(c)
+
+
+        return "".join(stack)
+
+
+def main():
+       n = Solution()
+       sl = n.makeGood( "abBAcC")
+       print(sl)
+
+if __name__ == "__main__":
+        main()        
+          
+
+
+

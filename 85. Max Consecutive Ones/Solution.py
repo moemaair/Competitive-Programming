7550db063c54d3c8897357a0e889c972b09892a8
moemaair <rageali12@gmail.com>
2023-10-08 13:44:11 +0300

    Runtime: 322ms space 16.42MB

---
+++ 85. Max Consecutive Ones/Solution.py
@@ -0,0 +1,15 @@
+from ast import List
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        count = 0
+        max_count = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count+=1
+            else:
+                count = 0   
+            max_count = max(max_count, count)        
+        return max_count        
+        
